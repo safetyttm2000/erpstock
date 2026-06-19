a6f442ed947a30d126af0ba6fc4ea7e72538a06d
safetyttm2000 <safety.ttm2000@gmail.com>
2026-06-19 23:32:31 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
 // After deploying the Apps Script project as a Web App, paste the /exec URL here.
 // Example: https://script.google.com/macros/s/AKfycb.../exec
 window.APP_CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyTh3Tkz8DS9k4Rb-VwBJgFonfft-KoSJOArRJYxe6JChKtHq4u54q9ki4l95AeY93Y/exec',
   LOGO_URL: 'https://img2.pic.in.th/ERP-Logo.png',
   SYSTEM_NAME: 'ERP Stock System'
 };
