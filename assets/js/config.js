f57e63e6acda406b6c6ea2467b1d853472e5b0bd
safetyttm2000 <safety.ttm2000@gmail.com>
2026-06-22 20:35:19 +0700

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-// After deploying the Apps Script project as a Web App, paste the /exec URL here.
-// Example: https://script.google.com/macros/s/AKfycb.../exec
-window.APP_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbyTh3Tkz8DS9k4Rb-VwBJgFonfft-KoSJOArRJYxe6JChKtHq4u54q9ki4l95AeY93Y/exec',
-  LOGO_URL: 'https://img2.pic.in.th/ERP-Logo.png',
-  SYSTEM_NAME: 'ERP Stock System'
-};
